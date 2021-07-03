a9f4e7e09b1fd898a11babfae306bda6a218925b
nirubanchakaravarthy <niruban.chakaravarthy@gmail.com>
2021-07-03 13:49:39 +0530

    First Commit

---
+++ src/test/java/sampleTest1/SampleTest1.java
@@ -0,0 +1,32 @@
+package sampleTest1;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest1 {
+	
+	@Test
+	public void loginTest() throws IOException{
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd= new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.get("https://www.google.com/");
+		
+		// interfaces == webdriver, takescreenshot , outputType
+		
+File src= ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		
+		FileHandler.copy(src, new File("C:\\seleniumWorkspace\\JenkinsGitTest\\ScreenShot\\Google.JPG"));
+
+	}
+
+}
